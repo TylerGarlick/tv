a187a78e438f640a2f279b3d901dd2e72eb1597c
Tyler Garlick <tyler-the-robot@outlook.com>
2023-01-30 23:13:05 -0700

    Added link to background.mp4

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import * as React from "react"
+import * as React from 'react'
 import { useRef } from 'react'
 import styled from 'styled-components'
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from '../components/layout'
+import Seo from '../components/seo'
 
 import videos from '../data/videos.json'
 
@@ -12,32 +12,35 @@ import backgroundSource from '../videos/background.mp4'
 console.log(backgroundSource)
 
 
-
 const Video = styled.video`
   width: 100%;
   height: 100%;
 `
 
+
 const IndexPage = () => {
   const videoRef = useRef(null)
   const video = videoRef.current
 
-  if (video) {
-    if (!video.isPlaying) {
-      video.play()
-    }
-
-    if (video.requestFullscreen) {
-      video.requestFullscreen()
-    }
-  }
+  // if (video) {
+  //   if (!video.isPlaying) {
+  //     video.play()
+  //   }
+  //
+  //   if (video.requestFullscreen) {
+  //     video.requestFullscreen()
+  //   }
+  // }
 
   return (
     <Layout>
-      <Seo title="Home" />
-      <Video controls autoPlay loop ref={videoRef}>
-        <source src="../videos/background.mp4" type="video/mp4" />
-        {/* {videos.map(({ src, type }, index) => <source src={src} key={index} type={type} />)} */}
+      <Seo title='Home' />
+      <Video controls autoPlay loop ref={ videoRef }
+             muted={ false }
+             width={ '100%' }
+             height={ '100%' }>
+        <source src='/static/background-cf0007305fc319aaf64ec4eccf04a8bd.mp4' type='video/mp4' />
+        {/* {videos.map(({ src, type }, index) => <source src={src} key={index} type={type} />)} */ }
 
         {/* <source
           src="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"
@@ -46,7 +49,7 @@ const IndexPage = () => {
         <source
           src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
           type="video/mp4"
-        /> */}
+        /> */ }
       </Video>
     </Layout>
   )
